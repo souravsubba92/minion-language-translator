eb2c638af6090777cd06d2b48cdf5412212748de
souravsubba92 <104068522+souravsubba92@users.noreply.github.com>
2023-06-07 14:27:30 +0530

    wired a div

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var btnTranslate = document.querySelector("#button");
 var textInput = document.querySelector("#input");
+var textOutput = document.querySelector("#output");
 
 function clickHandler() {
   console.log("Clicked");
   console.log(textInput.value);
+  textOutput.innerText = textInput.value;
 }
 btnTranslate.addEventListener("click", clickHandler);
