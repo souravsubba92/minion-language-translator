27b3f4a8f3eedf437df7269e35efb6f654beccb9
souravsubba92 <104068522+souravsubba92@users.noreply.github.com>
2023-06-07 15:01:38 +0530

    fetching from server

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ var btnTranslate = document.querySelector("#button");
 var textInput = document.querySelector("#input");
 var textOutput = document.querySelector("#output");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getUrl(text) {
+  return serverURL + "?text=" + text;
+}
+
 function clickHandler() {
-  console.log("Clicked");
-  console.log(textInput.value);
-  textOutput.innerText = textInput.value;
+  var input = textInput.value;
+  fetch(getUrl(input))
+    .then((response) => response.json())
+    .then((result) => {
+      textOutput.innerHTML = result.contents.translated;
+    });
 }
 btnTranslate.addEventListener("click", clickHandler);
